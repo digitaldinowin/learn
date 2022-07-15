843b071ae00e6c8ecab0b4d8155cdfa0c852eea6
Tao Jiang <tjiang@infomedia.com.au>
2022-07-15 14:42:20 +1000

    Add test case

--- sample/unitTest/calculator.py
+++ sample/unitTest/calculator.py
@@ -1,9 +1,22 @@
 class Calculator:
-  def method1(self, a, b, c):
+  def checker1(a, b, c):
     return a + b < c
 
-  def method2(self, a, b, c):
+  def checker2(a, b, c):
     if b != 0:
       return a / b < c
     else:
       return False
+  
+  def calculate(self, m, n, s):
+    t = 0
+    if Calculator.checker1(m, n, s):
+      t = 1.12
+    elif Calculator.checker2(m, n, s):
+      t = 2.07
+
+    return t
+
+if __name__ == '__main__':
+  t = Calculator().calculate(1, 1, 2)
+  print('%.1f' % t)
--- sample/unitTest/testCalculator.py
+++ sample/unitTest/testCalculator.py
@@ -4,13 +4,16 @@ from calculator import Calculator
 class TestCalculator(unittest.TestCase):
   calculator = Calculator()
 
-  def testMethod1(self):
-    self.assertFalse(self.calculator.method1(1, 1, 2))
-    self.assertTrue(self.calculator.method1(1, 0, 2))
+  def testChecker1(self):
+    self.assertFalse(Calculator.checker1(1, 1, 2))
+    self.assertTrue(Calculator.checker1(1, 0, 2))
 
-  def testMethod2(self):
-    self.assertTrue(self.calculator.method2(1, 1, 2))
-    self.assertFalse(self.calculator.method2(1, 0, 2))
+  def testChecker2(self):
+    self.assertTrue(Calculator.checker2(1, 1, 2))
+    self.assertFalse(Calculator.checker2(1, 0, 2))
+
+  def testCalculate(self):
+    return False
 
 if __name__ == '__main__':
   unittest.main()
