5c906b87182518e18c7ef177d2e97d703ab95c50
LuningW2010 <107750079+LuningW2010@users.noreply.github.com>
2022-07-16 20:12:52 +1000

    Update LineFollower.py

--- luning/LineFollower.py
+++ luning/LineFollower.py
@@ -7,9 +7,10 @@ hub = PrimeHub()
 hub.light_matrix.show_image('HAPPY')
 motor=MotorPair('E','F')
 color=ColorSensor('A')
+colorcheck=color.get_color()
 power=20
 while 1!=2 :
-    if color!='red':
-        motor.start_tank_at_power(7.5,power)
-    if color=='red':
-        motor.start_tank_at_power(7.5,power)
+    if colorcheck!='red':
+        motor.start_tank_at_power(10,7)
+    if colorcheck=='red':
+        motor.start_tank_at_power(7,10)
